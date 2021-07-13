85a7392e1255c361e06a2f85674fc5a39640d4dd
truongquanghung <truongquanghunghn@gmail.com>
2021-07-13 09:23:01 +0700

    Add build_model

--- main.py
+++ main.py
@@ -16,10 +16,10 @@ lemmatizer = WordNetLemmatizer()
 THRESHOLD = 0.25
 
 
-model = load_model('/code/resources/models/chatbot_model.h5')
-intents = json.loads(open('/code/resources/data/intents.json').read())
-words = pickle.load(open('/code/resources/pickles/words.pkl', 'rb'))
-classes = pickle.load(open('/code/resources/pickles/classes.pkl', 'rb'))
+model = load_model('resources/models/chatbot_model.h5')
+intents = json.loads(open('resources/data/intents.json').read())
+words = pickle.load(open('resources/pickles/words.pkl', 'rb'))
+classes = pickle.load(open('resources/pickles/classes.pkl', 'rb'))
 
 print('RESOURCES LOADED SUCESSFULLY!')
 
